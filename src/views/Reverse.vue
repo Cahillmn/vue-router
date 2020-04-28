cc58d45d2b4609cd0586a0037a21df9419a0580b
Cahillmn <mcahill@seattleu.edu>
2020-04-28 16:05:03 -0700

    finished vue-router

---
+++ src/views/Reverse.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="reverse">
+    <h1>This is the Reverse Page</h1>
+    <h2>Enter your string to reverse:</h2>
+    <label>String:
+      <input type="text" id="string" v-model.lazy="message">
+    </label>
+    <h2>the reversed message is:</h2>
+    <p> {{ reversedMessage }} </p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Reverse',
+    data (){
+      return {
+        message: ''
+      }
+    },
+  computed: {
+    reversedMessage() {
+      return this.message.split('').reverse().join('');
+
+    }
+  }
+
+}
+</script>
